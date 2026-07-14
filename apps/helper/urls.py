49cdde64296f7aaa40d80d934fbd12948261752a
bigdevlight <bigdevlight@gmail.com>
2026-07-14 12:49:13 +0900

    wip: 중간 점검

--- apps/helper/urls.py
+++ apps/helper/urls.py
@@ -5,7 +5,8 @@ from . import views
 app_name = "helper"
 
 urlpatterns = [
-    path("", views.waiting_view, name="waiting"),
+    path("", views.main_view, name="main"),
+    path("waiting/", views.waiting_view, name="waiting"),
     path("login/", views.login_view, name="login"),
     path("signup/", views.signup_view, name="signup"),
     path("logout/", views.logout_view, name="logout"),
