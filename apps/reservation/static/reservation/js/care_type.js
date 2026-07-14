592b591c642cc9b8fcd728947d413f9990080536
borissal1207 <borissal127@gmail.com>
2026-07-14 09:34:37 +0900

    feat: sos 위젯, 예약 플로우 화면 마무리

--- apps/reservation/static/reservation/js/care_type.js
+++ apps/reservation/static/reservation/js/care_type.js
@@ -98,19 +98,3 @@ confirmBtn.addEventListener("click", () => {
 
 renderTabs();
 renderSymptoms();
-
-const reviewChip = document.getElementById("reviewChip");
-
-function handleReviewChipActivate() {
-  console.log(".");
-}
-
-if (reviewChip) {
-  reviewChip.addEventListener("click", handleReviewChipActivate);
-  reviewChip.addEventListener("keydown", (e) => {
-    if (e.key === "Enter" || e.key === " ") {
-      e.preventDefault();
-      handleReviewChipActivate();
-    }
-  });
-}
