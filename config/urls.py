a8f4528e098aa5fead76a7cf96a2108a16ccb0b6
seunngh <jungshyun0615@gmail.com>
2026-07-14 13:01:16 +0900

    fix: 프로필 사진 반영되도록 수정

--- config/urls.py
+++ config/urls.py
@@ -19,5 +19,4 @@ urlpatterns = [
     path("api/accounts/", include("apps.accounts.urls")),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
